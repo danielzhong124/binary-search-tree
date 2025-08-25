8b27e1c842b263d91a3abe98b22d5a4c740f68f1
Daniel Zhong <44526047+danielzhong124@users.noreply.github.com>
2025-08-25 15:57:22 -0400

    Update tree.rb

--- lib/tree.rb
+++ lib/tree.rb
@@ -3,4 +3,27 @@
 require_relative 'node'
 
 class Tree
+  attr_reader :root
+
+  def initialize(array)
+    @root = build_tree(array.sort.uniq)
+  end
+
+  def build_tree(array)
+    return nil if array.empty?
+
+    mid = (array.length - 1) / 2
+
+    root = Node.new(array[mid])
+    root.left = build_tree(array[0...mid])
+    root.right = build_tree(array[(mid + 1)...array.length])
+
+    root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
 end
