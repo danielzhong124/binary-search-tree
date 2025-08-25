52bee9f0668cfa9249380d36a968fa0f58bbe7c7
Daniel Zhong <44526047+danielzhong124@users.noreply.github.com>
2025-08-25 15:57:15 -0400

    Update node.rb

--- lib/node.rb
+++ lib/node.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class Node
+  include Comparable
+  attr_reader :data
+  attr_accessor :left, :right
+
+  def initialize(data)
+    @data = data
+  end
+
+  def <=>(other)
+    @data <=> other.data
+  end
 end
